8578795303c6378c4670b68295cc2db6f45207de
Cristian Dumitru <cristiandumitr@gmail.com>
2019-10-05 18:42:49 +0300

    firebase: connect to firestore

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const express = require('express');
-const fs = require('fs');
 const bodyParser = require('body-parser');
+const cors = require('cors')({origin: true});
+
+const serviceAccount = require('./config/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://users-management-angular.firebaseio.com"
+});
+
+const db = admin.firestore();
+const usersDocRef = db.collection('users');
 
 const app = express();
+
+app.use(cors);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,96 +26,91 @@ app.use(bodyParser.json());
 
 app.use('/', express.static(__dirname + '/dist/user-management/'));
 
-const dbfile = '../backend/table.json';
-
-//dummy implementation of unique id
-function getRandomId(keys) {
-  let id = "id_0";
-
-  while (keys.indexOf(id) > -1) {
-    id = 'id_' + (Math.floor(Math.random() * 1000) + 1);
-  }
-  return id;
-}
-
 app.get('/table/user/:id', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
-
-  fs.readFile(dbfile, 'utf8', function (err, data) {
-    data = JSON.parse(data);
-    const response = data[req.params.id];
-    res.end(JSON.stringify(response));
-  });
+  const id = req.params.id;
+  usersDocRef
+    .getById()
+    .doc(id)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        console.log('Document data:', doc.data());
+      }
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
 });
 
 app.get('/table/users', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
 
-  fs.readFile(dbfile, 'utf8', function (err, data) {
-    data = JSON.parse(data);
-    const response = { rows: data };
+  usersDocRef
+    .get()
+    .then(snapshot => {
+      const response = { rows: {} };
 
-    res.end(JSON.stringify(response));
-  });
+      if (snapshot.empty) {
+        console.log('No matching documents.');
+      }
+
+      snapshot.forEach(doc => {
+        response.rows[doc.id] = {...doc.data(), id: doc.id};
+      });
+
+      res.end(JSON.stringify(response));
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
 });
 
 app.post('/table/create/user', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
 
-  fs.readFile(dbfile, 'utf8', function (err, data) {
-
-    data = JSON.parse(data);
-
-    const newUser = req.body;
-    newUser.id = getRandomId(Object.keys(data));
-
-    data[newUser.id] = newUser;
-
-    fs.writeFile(dbfile, JSON.stringify(data), function (err) {
+  usersDocRef
+    .add(req.body)
+    .then(ref => {
+      console.log('Added document with ID: ', ref.id);
       res.end(JSON.stringify({
         messages: {
           success: 'Added new user'
         }
       }));
     });
-  });
 });
 
 app.put('/table/update/user/:id', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
 
-  fs.readFile(dbfile, 'utf8', function (err, data) {
+  const id = req.params.id;
 
-    data = JSON.parse(data);
-
-    data[req.params.id] = req.body;
-
-    fs.writeFile(dbfile, JSON.stringify(data), function (err) {
+  usersDocRef
+    .doc(id)
+    .update(req.body)
+    .then(ref => {
+      console.log('Updated document with ID: ', ref.id);
       res.end(JSON.stringify({
         messages: {
           success: 'Updated user'
         }
       }));
     });
-  });
 });
 
 app.delete('/table/delete/user/:id', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
-
-  fs.readFile(dbfile, 'utf8', function (err, data) {
-
-    data = JSON.parse(data);
-
-    delete data[req.params.id];
-
-    fs.writeFile(dbfile, JSON.stringify(data), function (err) {
-      res.end(JSON.stringify({
-        messages: {
-          success: 'Removed user'
-        }
-      }));
-    });
+  const id = req.params.id;
+  usersDocRef.doc(id).delete().then(ref => {
+    console.log('Deleted document with ID: ', ref.id);
+    res.end(JSON.stringify({
+      messages: {
+        success: 'Removed user'
+      }
+    }));
   });
 });
 
